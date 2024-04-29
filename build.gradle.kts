08e4324b20b651f81dc528180761a473f4cd4c11
hytsnbr10519 <haya524mwam@gmail.com>
2024-04-29 09:27:54 +0900

    feat(gradle): Version Catalogを導入した

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("java")
     id("idea")
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
+    alias(libs.plugins.springframework.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
 group = "com.hytsnbr"
@@ -19,32 +19,32 @@ repositories {
 
 dependencies {
     // Spring Boot Starter
-    implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation(libs.spring.boot.starter.batch)
+    implementation(libs.spring.boot.starter.validation)
 
     // Spring Boot Configuration Processor
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor(libs.spring.boot.configuration.processor)
 
     // Apache Commons
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation(libs.commons.lang3)
+    implementation(libs.commons.collections4)
 
     // lombok
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
 
     // H2 Database
-    runtimeOnly("com.h2database:h2")
+    runtimeOnly(libs.h2)
 
     // Jsoup Java HTML Parser
-    implementation("org.jsoup:jsoup:1.17.2")
+    implementation(libs.jsoup)
 
     // Jackson Datatype: JSR310（java.time系の読み込み用）
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation(libs.jackson.datatype.jsr310)
 
     // Unit Test
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.batch:spring-batch-test")
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.batch.test)
 }
 
 tasks.test {
