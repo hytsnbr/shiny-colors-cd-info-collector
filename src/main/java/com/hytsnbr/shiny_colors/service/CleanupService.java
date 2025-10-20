6bf9b3497f01d5af698de46f2253356e5b0cbf66
hytsnbr <17739319+hytsnbr@users.noreply.github.com>
2025-10-20 12:38:01 +0900

    test

---
+++ src/main/java/com/hytsnbr/shiny_colors/service/CleanupService.java
@@ -0,0 +1,8 @@
+package com.hytsnbr.shiny_colors.service;
+
+public class CleanupService  {
+
+  private static final Logger logger = LoggerFactory.getLogger(CleanupService.class);
+
+  public boolean 
+}
---
+++ src/main/java/com/hytsnbr/shiny_colors/service/TaskletService.java
@@ -0,0 +1,4 @@
+public interface TaskletService {
+
+  boolean execute(String... args);
+}
